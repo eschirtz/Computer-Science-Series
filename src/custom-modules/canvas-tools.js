c1a140018383d20cfb7d83440e4b11fea0dd3b83
Eric Schirtzinger <eschirtzinger@gmail.com>
2019-01-23 21:34:11 -0600

    One more linty boi

--- src/custom-modules/canvas-tools.js
+++ src/custom-modules/canvas-tools.js
@@ -1,32 +1,32 @@
-function getRetinaRatio(canvas) {
+function getRetinaRatio (canvas) {
   /* eslint-disable no-console */
-  if (!canvas) { console.warn('Canvas required!'); }
+  if (!canvas) { console.warn('Canvas required!') }
   /* eslint-enable no-console */
-  const devicePixelRatio = window.devicePixelRatio || 1;
-  const c = canvas;
+  const devicePixelRatio = window.devicePixelRatio || 1
+  const c = canvas
   const backingStoreRatio = [
     c.webkitBackingStorePixelRatio,
     c.mozBackingStorePixelRatio,
     c.msBackingStorePixelRatio,
     c.oBackingStorePixelRatio,
     c.backingStorePixelRatio,
-    1,
-  ].reduce((a, b) => a || b);
+    1
+  ].reduce((a, b) => a || b)
 
-  return devicePixelRatio / backingStoreRatio;
+  return devicePixelRatio / backingStoreRatio
 }
 
-function setCanvasSize(canvas) {
-  const ratio = getRetinaRatio(canvas);
+function setCanvasSize (canvas) {
+  const ratio = getRetinaRatio(canvas)
   /* eslint-disable no-param-reassign */
-  canvas.width = window.innerWidth * ratio;
-  canvas.height = window.innerHeight * ratio;
-  canvas.style.width = `${window.innerWidth}px`;
-  canvas.style.height = `${window.innerHeight}px`;
+  canvas.width = window.innerWidth * ratio
+  canvas.height = window.innerHeight * ratio
+  canvas.style.width = `${window.innerWidth}px`
+  canvas.style.height = `${window.innerHeight}px`
   /* eslint-enable no-param-reassign */
 }
 
 export default {
   getRetinaRatio,
-  setCanvasSize,
-};
+  setCanvasSize
+}

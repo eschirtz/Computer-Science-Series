7da2a4e40291344d5388304a196083d8b4bdcb8c
tsteiner4 <tristan.steiner33@gmail.com>
2019-01-23 22:53:21 -0600

    Made some nicer distributions

--- src/views/ep03/js/utility.js
+++ src/views/ep03/js/utility.js
@@ -16,7 +16,7 @@ function getAveragePos (bucket) {
     y: ySum / length
   }
 }
-
+/*
 function getGroupedDistribution (width, height, numPoints, numGroups) {
   const points = []
   const groups = []
@@ -38,4 +38,59 @@ function getGroupedDistribution (width, height, numPoints, numGroups) {
   }
   return points
 }
+
+*/
+
+function getGroupedDistribution (width, height, numPoints, numGroups) {
+  const points = []
+  const groups = []
+  const PADDING = 100
+  let xDistr = 100
+  let yDistr = 100
+
+  let tempX, tempY
+  for (let i = 0; i < numGroups; i += 1) {
+    xDistr = 100 * (Math.random() + 1)
+    yDistr = 100 * (Math.random() + 1)
+    groups.push(
+      {
+	x: (Math.random() * (width - PADDING)),
+	y: (Math.random() * (height - PADDING))
+      }
+    )
+    for (let j = 0; j < numPoints; j += 1) {
+      let g1 = getGauss() * xDistr
+      let g2 = getGauss() * yDistr
+      if (groups[i].x + g1 > width) {
+        tempX = groups[i].x - g1
+      } else {
+	tempX = groups[i].x - g1
+      }
+
+      if (groups[i].y + g2 > height) {
+	tempY = groups[i].y - g2
+      } else {
+	tempY = groups[i].y + g2
+      }
+
+      points.push(
+        {
+	x: tempX,
+	y: tempY
+        }
+      )
+    }
+  }
+  return points
+}
+
+function getGauss() {
+  let u = 0
+  let v = 0
+  while (u === 0) u = Math.random()
+  while (v === 0) v = Math.random()
+  return Math.sqrt( -2.0 * Math.log( u )) * Math.cos( 2.0 * Math.PI * v)
+}
+
+
 export default { getDistance, getAveragePos, getGroupedDistribution }
